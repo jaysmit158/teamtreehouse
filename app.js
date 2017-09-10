988eb6f5d881c17ddb3a91797679526c810d34ed
JSmith <jaysmit158@gmail.com>
2017-09-10 18:01:04 -0400

    made response status code for errors in plain english using http module

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const http = require('http');
 // Problem: We need a simple way to look at a user's badge count and JavaScript points
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 
@@ -13,29 +14,35 @@ function printMessage(username, badgeCount, points, javascript) {
 
 function getProfile(username) {
   try {
-  // Connect to the API url (https://teamtreehouse.com/username.json)
-  const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-    let body = "";
-    //Read the data
-    response.on('data', data => {
-      body += data.toString();
-    });
+    // Connect to the API url (https://teamtreehouse.com/username.json)
+    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+      if (response.statusCode === 200) {
+        let body = "";
+        //Read the data
+        response.on('data', data => {
+          body += data.toString();
+        });
 
-    response.on('end', () => {
-      try {
-        //Parse the data
-        const profile = JSON.parse(body);
-        //Print data
-        printMessage(username, profile.badges.length, profile.points.total, profile.points.JavaScript);
-      } catch (error) {
-        printError(error)
+        response.on('end', () => {
+          try {
+            //Parse the data
+            const profile = JSON.parse(body);
+            //Print data
+            printMessage(username, profile.badges.length, profile.points.total, profile.points.JavaScript);
+          } catch (error) {
+            printError(error)
+          }
+        });
+      } else {
+        const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
+        const statusCodeError = new Error(message);
+        printError(statusCodeError);
       }
     });
-  });
-  request.on('error', printError;
-} catch (error) {
-  printError(error);
-}
+    request.on('error', printError);
+  } catch (error) {
+    printError(error);
+  }
 
 }
 
